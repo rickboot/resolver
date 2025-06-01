b6a6cf5ad231ad7fe4d741e319951e613041dcb7
Rick Allen <rick@Ricks-MacBook-Air.local>
2025-05-31 23:01:06 -0700

    frontend ux now calls api

--- backend/api/main.py
+++ backend/api/main.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from backend.scraper.web_scraper import scrape_website
 from backend.llm.factory import get_llm_client
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 llm_client = get_llm_client()
 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,57 @@
+'use client'
+
+import { useState } from "react";
 
 export default function Home() {
+  const [url, setUrl] = useState('');
+  const [result, setResult] = useState<any | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setResult(null);
+  
+    try {
+      const response = await fetch('http://localhost:8000/analyze', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ url }),
+      });
+
+      const data = await response.json();
+      setResult(data);
+    } catch (err) {
+      setResult({error: err});
+    }
+    finally {
+    setLoading(false);
+  }
+  }
+
+
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="flex-1 flex justify-center items-center">
+      <main className="flex-1 flex flex-col justify-center items-center">
         <div className="flex flex-col items-center w-160">
-          <h1 className="text-4xl font-bold">Resolver</h1>
-          <h2 className="text-xl">On-brand marketing content creation for ecommerce</h2>
-
-          <label className="mt-4">Website URL</label>
-          <input type="url" className="border border-gray-300 rounded w-72 p-2 bg-foreground text-black" />
-          <button className="mt-4 px-4 py-2 bg-[var(--button-color)] text-black rounded">Sign Up</button>    
-
+          <form onSubmit={handleSubmit} className="flex flex-col items-center">
+            <h1 className="text-4xl font-bold">Resolver</h1>
+            <h2 className="text-xl">On-brand Marketing Content Creation</h2>
+            <label htmlFor="website-url" className="mt-4 text-black">Website URL</label>
+            <input id="website-url" placeholder='https://example.com' type="url" className="border border-gray-300 rounded w-64 p-2 bg-foreground text-black" onChange={(e) => setUrl(e.target.value)}/>
+            <button disabled={loading || !url} className="disabled:opacity-50 mt-4 px-4 py-2 bg-[var(--button-color)] text-black rounded" type="submit">{loading ? 'Loading...' : 'Submit'}</button>    
+          </form>
+          {result && (
+            <div className="mt-12 text-black w-96 text-center">
+              <h2 className="font-bold text-lg">Brand Information</h2>
+              <p className="mt-2" >{result.company_name}</p>
+              <p className="mt-2">{result.url}</p>
+              <p className="mt-2">{result.target_audience}</p>
+              <p className="mt-2">{result.writing_style}</p>
+            </div>
+          )}
         </div>
       </main>
       <footer className="flex justify-center items-center">
@@ -18,3 +59,15 @@ export default function Home() {
     </div>
   );
 }
+
+
+/*
+  "company_name": company_name,
+  "website_url": url,
+  "og_title": og_title,
+  "og_description": og_description,
+  "social_media_links": unique_social_media_links,
+  "brand_colors": unique_brand_colors,
+  "writing_style": writing_style,
+  "target_audience": target_audience
+*/
